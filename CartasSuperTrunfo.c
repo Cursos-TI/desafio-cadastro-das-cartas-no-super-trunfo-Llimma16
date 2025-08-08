1678a9af091f2e15a8794b4105672ffdf84d0853
Llimma16 <leo.limma@live.com>
2025-08-08 18:26:32 +0000

    Comparação

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -90,5 +90,63 @@ int main() {
     printf("PIB: %.2f bilhões\n", PIBB);
     printf("Números de Pontos Turísticos: %d\n", numpontosturistcsB);
 
+      printf("\n=========== Comparação ===========\n");
+
+if (codigoCartaA > codigoCartaB) {
+    printf("Carta A tem o maior código (%s > %s)\n", codigoCartaA, codigoCartaB);
+} else if (codigoCartaA < codigoCartaB) {
+    printf("Carta B tem o maior código (%s > %s)\n", codigoCartaB, codigoCartaA);
+} else {
+    printf("Os códigos são iguais (%s = %s)\n", codigoCartaA, codigoCartaB);
+}
+
+if (estadoA > estadoB) {
+    printf("Carta A tem um estado maior: (%c > %c)\n", estadoA, estadoB);
+} else if (estadoA < estadoB) {
+    printf("Carta B tem um estado maior: (%c > %c)\n", estadoB, estadoA);
+} else {
+    printf("Os estados têm valores que são iguais. (%c = %c)\n", estadoA, estadoB);
+}
+
+if (cidadeA > cidadeB) {
+    printf("Carta A tem o maior cidade: (%s > %s)\n", cidadeA, cidadeB);
+} else if (cidadeA < cidadeB) {
+    printf("Carta B tem o maior cidade: (%s > %s)\n", cidadeB, cidadeA);
+} else {
+    printf("As cidades têm mesmo valor:  (%s = %s)\n", cidadeA, cidadeB);
+}
+
+if (populacaoA > populacaoB) {
+    printf("Carta A tem a maior população: (%lu > %lu)\n", populacaoA, populacaoB);
+} else if (populacaoA < populacaoB) {
+    printf("Carta B tem a maior população: (%lu > %lu)\n", populacaoB, populacaoA);
+} else {
+    printf("A população tem valores iguais. (%lu = %lu)\n", populacaoA, populacaoB);
+}
+
+if (areaA > areaB) {
+    printf("Carta A tem a maior área: (%.2f > %.2f)\n", areaA, areaB);
+} else if (areaA < areaB) {
+    printf("Carta B tem o maior área: (%.2f > %.2f)\n", areaB, areaA);
+} else {
+    printf("As áreas têm valores iguais. (%.2f = %.2f)\n", areaA, areaB);
+}
+
+if (PIBA > PIBB) {
+    printf("Carta A tem o maior PIB: (%.2f > %.2f)\n", PIBA, PIBB);
+} else if (PIBA < PIBB) {
+    printf("Carta B tem o maior PIB: (%.2f > %.2f)\n", PIBB, PIBA);
+} else {
+    printf("Os valores de PIBs são iguais. (%.2f = %.2f)\n", PIBA, PIBB);
+}
+
+if (numpontosturistcsA > numpontosturistcsB) {
+    printf("Carta A tem o maior número de pontos turísticos: (%d > %d)\n", numpontosturistcsA, numpontosturistcsB);
+} else if (numpontosturistcsA < numpontosturistcsB) {
+    printf("Carta B tem o maior número de pontos turísticos: (%d > %d)\n", numpontosturistcsB, numpontosturistcsA);
+} else {
+    printf("Os valores dos pontos turísticos são iguais (%d = %d)\n", numpontosturistcsA, numpontosturistcsB);
+}
+
     return 0;
 }
